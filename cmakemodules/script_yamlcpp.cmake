28ba4a88345d271fe525cf617a031049695962f8
Nicolas Scheidt <nicoscheidt@gmx.de>
2019-08-28 14:10:41 +0200

    yaml cpp catkin fix

--- cmakemodules/script_yamlcpp.cmake
+++ cmakemodules/script_yamlcpp.cmake
@@ -1,5 +1,5 @@
 # Check for yaml-cpp libray.
-# It has a *-config.cmake files, but they are not present in Ubuntu 12.04 LTS, 
+# It has a *-config.cmake files, but they are not present in Ubuntu 12.04 LTS,
 # so we use pkg-config there.
 # ===================================================
 set(CMAKE_MRPT_HAS_YAMLCPP 0)
@@ -10,21 +10,14 @@ set(CMAKE_MRPT_HAS_YAMLCPP_SYSTEM 0)
 option(DISABLE_YAMLCPP "Force not using yamlcpp" "OFF")
 mark_as_advanced(DISABLE_YAMLCPP)
 if(NOT DISABLE_YAMLCPP)
-	# Invoke pkg-config for getting the configuration:
-	if(PKG_CONFIG_FOUND)
-		PKG_CHECK_MODULES(LIBYAMLCPP QUIET yaml-cpp)
-		if (LIBYAMLCPP_FOUND)
-			set(CMAKE_MRPT_HAS_YAMLCPP 1)
-			set(CMAKE_MRPT_HAS_YAMLCPP_SYSTEM 1)
-
-			include_directories(${LIBYAMLCPP_INCLUDE_DIRS})
-			if($ENV{VERBOSE})
-				message(STATUS "LIBYAMLCPP_LIBRARIES    : ${LIBYAMLCPP_LIBRARIES}")
-				message(STATUS "LIBYAMLCPP_INCLUDE_DIRS : ${LIBYAMLCPP_INCLUDE_DIRS}")
-				message(STATUS "LIBYAMLCPP_VERSION      : ${LIBYAMLCPP_VERSION}")
-			endif()
-		endif()
+	FIND_PACKAGE(PkgConfig REQUIRED)
+	find_package(yaml_cpp_catkin QUIET)
+	if(${yaml_cpp_catkin_FOUND})
+		message(STATUS "Found yaml_cpp_catkin, using instead of system library.")
+		set(YamlCpp_LIBRARIES ${yaml_cpp_catkin_LIBRARIES})
+		set(YamlCpp_INCLUDE_DIRS ${yaml_cpp_catkin_INCLUDE_DIRS})
+	else()
+		message(STATUS "No yaml_cpp_catkin, using yaml-cpp system library instead.")
+		pkg_check_modules(YamlCpp REQUIRED yaml-cpp>=0.5)
 	endif()
-
 endif()
-
