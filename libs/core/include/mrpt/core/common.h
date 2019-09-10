bb299760af9f1a5a6385ef28166be947f35d9f77
Nicolas Scheidt <nicoscheidt@gmx.de>
2019-09-10 10:22:26 +0200

    actually temporarily disable MRPT_TODO compile time warnings

--- libs/core/include/mrpt/core/common.h
+++ libs/core/include/mrpt/core/common.h
@@ -126,7 +126,7 @@ need to account for this with an extra offset.
 #endif
 
 #define MRPT_WARNING(x) MRPT_MSG_PRAGMA("Warning: " x)
-#define MRPT_TODO(x) do {} while(0)
+#define MRPT_TODO(x)
 
 // Define a decl. modifier for printf-like format checks at compile time:
 #ifdef __GNUC__
